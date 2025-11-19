b9b62e55a0d1d7b67e238996ba0e60c6eeef9af6
nunkesser <robin.nunkesser@hshl.de>
2025-11-19 12:30:40 +0100

    Modified widget test

--- mas_test_app/test/widget_test.dart
+++ mas_test_app/test/widget_test.dart
@@ -9,22 +9,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:mas_test_app/main.dart';
+import 'package:mas_test_app/result.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Start test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      ChangeNotifierProvider(
+        create: (context) => Result(),
+        child: const MyApp(),
+      ),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.widgetWithText(ElevatedButton, 'Start'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Tap the Start button and trigger a frame.
+    await tester.tap(find.widgetWithText(ElevatedButton, 'Start'));
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
